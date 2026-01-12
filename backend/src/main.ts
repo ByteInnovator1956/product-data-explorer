03bda61bf32945c69e16a552db15547c6dae3818
ByteInnovator1956 <sourabhtagad3@gmail.com>
2026-01-12 19:41:15 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,16 @@
 process.env.CRAWLEE_DISABLE_MEMORY_SNAPSHOT = '1';
 process.env.CRAWLEE_DISABLE_SNAPSHOT = '1';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000
-  await app.listen(port)
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-  console.log(` Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 }
-bootstrap()
+
+bootstrap();
